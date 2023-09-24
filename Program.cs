c0dc31c2a27413017756bf5d22b0237eda669b2d
Keoma Kevin Baudin <keoma4@gmail.com>
2023-09-23 23:47:20 -0300

    feat: implementa testes com as classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "98888-8888", modelo: "C12", imei: "111111-11-111111-1", memoria: 64);
+Smartphone iphone = new Iphone(numero: "97777-7777", modelo: "15 Pro", imei: "222222-22-222222-2", memoria: 128);
+
+Console.WriteLine("Smartphone Nokia:");
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone iPhone:");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
